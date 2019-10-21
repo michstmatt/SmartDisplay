8fbdcc5efe33851da9ede777fcacbb06854d5493
Matt Pasco <michstmatt@gmail.com>
2019-10-20 23:23:22 -0600

    add getting current song

--- src/main.js
+++ src/main.js
@@ -1,24 +1,9 @@
-angular
-  .module('exampleApp', ['spotify'])
-  .config(function (SpotifyProvider) {
-    SpotifyProvider.setClientId('bb96a88f1128455f9793ae7f181747f3');
-    SpotifyProvider.setRedirectUri('http://localhost/redirect.html');
-    SpotifyProvider.setScope('playlist-read-private');
-  })
-  .controller('MainController', ['$scope', 'Spotify', function ($scope, Spotify) {
-
-    $scope.searchArtist = function () {
-      Spotify.search($scope.searchartist, 'artist').then(function (data) {
-        $scope.artists = data.artists.items;
-      });
-    };
-
-    $scope.login = function () {
-      Spotify.login().then(function (data) {
-        console.log(data);
-        alert("You are now logged in");
-      }, function () {
-        console.log('didn\'t log in');
-      })
-    };
-}]);
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
